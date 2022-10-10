06bb5fea90a4d8dd0b4ec4a4219a49f7fc75185e
ArArovych <kazkarhotko@gmail.com>
2022-10-11 01:09:08 +0300

    WIP Added slyder

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
-// const watch = require('gulp-watch');
 
 gulp.task('sass-compile', function() {
     return gulp.src('./scss/**/*.scss')
---
+++ js/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows:false,
+        dots:true,
+        autoplay:true,
+        autoplaySpeed:3000,
+    });
+});
