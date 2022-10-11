a2d6c300a4b1a10742eebc27c18afa2bdb32b9af
ArArovych <kazkarhotko@gmail.com>
2022-10-12 01:03:56 +0300

    Added more adaptive for slyder and perfect-pixel

--- js/script.js
+++ js/script.js
@@ -4,7 +4,9 @@ $(document).ready(function(){
     $('.slider').slick({
         arrows:false,
         dots:true,
+        easing:'ease',
         autoplay:true,
         autoplaySpeed:3000,
+        waitForAnimate:false,
     });
 });
