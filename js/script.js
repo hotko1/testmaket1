c79b706f6503c34ead32da8e8f250488b47d466e
ArArovych <kazkarhotko@gmail.com>
2022-11-03 00:11:23 +0200

    Added scrolling after clicking a menu item and fixed burger menu style

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /**
  * @file
- * Slider, burger menu and like button behaviours.
+ * Slider, burger menu, like button and menu behaviours.
  */
 
 'use strict';
@@ -23,6 +23,20 @@ $(document).ready(function(){
         $('.header-burger,.header-menu').toggleClass('active');
         $('body').toggleClass('lock');
     });
+
+    // Delete class active and class lock after clicking a menu item.
+    $('a[href*="#"]').click(function(ev) {
+
+        const unactiveBurger = document.querySelector('.header-burger');
+        const unactiveMenu = document.querySelector('.header-menu');
+
+        unactiveBurger.classList.remove('active');
+        unactiveMenu.classList.remove('active');
+
+        const unlock = document.querySelector('body');
+
+        unlock.classList.remove('lock');
+    });
 });
 
 // Behaviours for the heart icons and counter in gallery.
@@ -43,3 +57,23 @@ function heart(x) {
     const newCounter = "#counter-" + i;
     $(newCounter).text(numCounter);
 }
+
+// Scrolling behavior after clicking a menu item.
+document.querySelectorAll('a[href*="#"]').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault()
+
+        const href = this.getAttribute('href').substring(1);
+
+        const scrollTarget = document.getElementById(href);
+
+        const topOffset = document.querySelector('.header').offsetHeight;
+        const elementPosition = scrollTarget.getBoundingClientRect().top;
+        const offsetPosition = elementPosition - topOffset;
+
+        window.scrollBy({
+            top: offsetPosition,
+            behavior: 'smooth'
+        })
+    })
+})
